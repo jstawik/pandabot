225bab9dadb374a590c5ae3361161d7f08ccccd6
Panda Academy <kontakt@pandait.pl>
2020-11-29 12:46:22 +0000

    Healthcheck in separate file

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 import discord
 from discord.ext import commands
 from discord_handler import DiscordHandler
-import discord_logging
 import json
 import logging
 import os
 import random
-from quart import Quart
-app = Quart(__name__)
+import healthcheck 
 
-with open( f"{os.path.dirname(__file__)}/config.json", 'r') as file:
+config_path = "/home/kstawik/.pandacfg"
+
+with open( f"{config_path}/config.json", 'r') as file:
     config = json.loads(file.read())
 
 pfx = config["prefix"]
@@ -18,26 +18,32 @@ bot = commands.Bot(command_prefix=pfx, intents=discord.Intents.all())
 
 @bot.event
 async def on_ready():
-    logger.info(f'We have logged in as {bot.user}')
+    logger.DEBUG(f'We have logged in as {bot.user}')
     global guild
     guild = [guild for guild in bot.guilds if guild.id == config["server_id"]][0]
-    
+
+# @client.event
+# async def on_member_join(member):
+#     await member.send("Welcome!")
+
 @bot.event
 async def on_member_update(before, after):
-    with open( f"{os.path.dirname(__file__)}/role_message.json", 'r') as file:
+    panda_role = [role for role in guild.roles if role.name == "Panda"][0]
+    with open( f"{config_path}/role_message.json", 'r') as file:
         role_message = json.loads(file.read())
     
     for role_name in config["role_names"]:
         role = [role for role in guild.roles if role.name == role_name][0]
         
-        if role in after.roles and role not in before.roles: 
+        if role in after.roles and role not in before.roles:
+            await after.add_roles(panda_role) 
             channel_name = f"{role.name}_{after}".lower().replace("#", "")
             
             if not channel_exists(channel_name):
                 channel_category = [category for category in guild.categories if category.name == config["bot_category_name"]][0]
                 channel = await create_text_channel(user=after, name=channel_name, category=channel_category)
                 if role_message[role.name]:
-                    logger.info(f"Sending a welcome message to a new {role.name}")
+                    logger.DEBUG(f"Sending a welcome message to a new {role.name}")
                     await channel.send(role_message[role.name])
 
 @bot.command(name="private_channel")
@@ -47,7 +53,7 @@ async def private_channel(ctx, user: str):
         user= guild.get_member_named(user)
         await create_text_channel(user=user, name=user, category=channel_category)
     except Exception as e:
-        logger.info (e)
+        logger.DEBUG (e)
         await ctx.send('Wystąpił błąd, prawdopodobnie nie znaleziono takiej osoby')
         return
 
@@ -67,29 +73,26 @@ async def create_text_channel(user, name, category):
     tmp_channel = await guild.create_text_channel(name=name, category=category)
     await tmp_channel.set_permissions(user, read_messages=True, send_messages=True)
 
-    logger.info(f"Channel {tmp_channel.name}"
+    logger.DEBUG(f"Channel {tmp_channel.name}"
           f" created in {tmp_channel.category}"
           f" with ID {tmp_channel.id}"
           f" on behalf of {user}")
     return tmp_channel
 
-@app.route('/health')
-async def hello():
-    return 'Alive'
 
 def init_logging():
     logging_format = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
     logging.basicConfig(filename='/home/kstawik/pandabot/discord.log' 
         , format=logging_format
-        , level=logging.INFO
+        , level=logging.DEBUG
     )
 
     discord_handler = DiscordHandler(config["logger_webhook"])
-    discord_handler.setLevel(logging.INFO)
+    discord_handler.setLevel(logging.DEBUG)
     discord_handler.setFormatter(logging.Formatter(logging_format))
 
     stream_handler = logging.StreamHandler()
-    stream_handler.setLevel(logging.INFO)
+    stream_handler.setLevel(logging.DEBUG)
     stream_handler.setFormatter(logging.Formatter(logging_format))
     
     logger.addHandler(discord_handler)
@@ -110,5 +113,5 @@ logger = logging.getLogger('discord')
 
 init_logging()
 
-bot.loop.create_task(app.run_task('0.0.0.0', 5000))
+bot.loop.create_task(healthcheck.app.run_task('0.0.0.0', 5000))
 bot.run(config["key"])
