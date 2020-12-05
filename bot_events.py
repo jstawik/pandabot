ad32cb553abcd5d7093bdf101aa4a4018b2c9f82
Panda Academy <kontakt@pandait.pl>
2020-12-05 17:10:26 +0000

    Questions generation

--- bot_events.py
+++ bot_events.py
@@ -1,9 +1,10 @@
 import logging
 import panda_tools
+import panda_roles
 from panda_init import *
 import json
 
-logger = init_logging(config, __name__) 
+logger = init_logging(config, __name__)
 
 @bot.event
 async def on_ready():
@@ -11,28 +12,11 @@ async def on_ready():
 
 @bot.event
 async def on_member_update(before, after):
-    guild = before.guild
+    logger.debug(f"Member update from {before} to {after}")
 
-    logger.debug(f"Member update from {before} to {after} ")
-    
-    with open( f"{config_path}/role_message.json", 'r') as file:
-        role_message = json.loads(file.read())
-    
-    for role_name in config["role_names"]:
-        logger.debug(f"Searching for the configured roles in guild roles: {guild.roles}")
-        role = [role for role in guild.roles if role.name == role_name][0]
-        
-        if role in after.roles and role not in before.roles:
-            logger.debug(f"Searching for the panda role in guild roles: {guild.roles}")
-            panda_role = [role for role in guild.roles if role.name == "Panda"][0]
-            await after.add_roles(panda_role)
-            channel_name = f"{role.name}_{after}".lower().replace("#", "")
-            logger.debug(f"Checking if channel {channel_name} exists")
-            
-            if not panda_tools.channel_exists(channel_name, guild):
-                logger.debug(f"Channel {channel_name} doesn't exist. Trying to create channel")
-                channel_category = [category for category in guild.categories if category.name == config["bot_category_name"]][0]
-                channel = await panda_tools.create_text_channel(guild=guild, user=after, name=channel_name, category=channel_category)
-                if role_message[role.name]:
-                    logger.info(f"Sending a welcome message to a new {role.name}")
-                    await channel.send(role_message[role.name])
+    if len(before.roles) < len(after.roles):
+        try:
+            await panda_roles.on_role_update(before, after)
+        except Exception as e:
+            logger.warning(e)
+            await panda_tools.channel_message(after.guild, e, "pandabot-log")
--- panda_tools.py
+++ panda_tools.py
@@ -4,7 +4,10 @@ from panda_init import *
 logger = init_logging(config, __name__) 
 
 async def create_text_channel(guild, user, name, category):
+    category = [cat for cat in guild.categories if cat.name == category][0]
+    logger.debug(f"Creating channel named {name} inside category {category}")
     tmp_channel = await guild.create_text_channel(name=name, category=category)
+    logger.debug(f"Setting permissions for user {user}")
     await tmp_channel.set_permissions(user, read_messages=True, send_messages=True)
 
     logger.info(f"Channel {tmp_channel.name}"
@@ -13,9 +16,27 @@ async def create_text_channel(guild, user, name, category):
           f" on behalf of {user}")
     return tmp_channel
     
+async def add_role(user, role_name):
+    guild = user.guild
+    logger.debug(f"Searching for the {role_name} role in guild roles: {guild.roles}")
+    role = [role for role in guild.roles if role.name == role_name][0]
+    await user.add_roles(role)
+
 def channel_exists(name, guild):
     channel_exists = False
     for channel in guild.channels:
         if channel.name == name:
             channel_exists = True 
     return channel_exists
+
+async def channel_message(guild, message, channel_name, embed=False):
+    channel = [channel for channel in guild.channels if channel.name == channel_name][0]
+    logger.debug(f"Printing message: {type(message)}")
+    if embed:
+        logger.debug(f"Printing embed message")
+        await channel.send(embed=message)
+    else:
+        logger.debug(f"Printing message: {message}")
+        await channel.send(message)
+    
+        
