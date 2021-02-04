52a1c29280e9f0ff998009c3b1f1f0f9bca358c2
root <root@stawik.dev>
2021-02-04 18:33:15 +0000

    Bot events - on_join, on_remove

--- bot_events.py
+++ bot_events.py
@@ -3,6 +3,7 @@ import panda_tools
 import panda_roles
 from panda_init import *
 import json
+from datetime import datetime
 
 logger = init_logging(config, __name__)
 
@@ -20,3 +21,29 @@ async def on_member_update(before, after):
         except Exception as e:
             logger.warning(e)
             await panda_tools.channel_message(after.guild, e, "pandabot-log")
+
+@bot.event
+async def on_member_join(member):
+    try:
+        logger.debug(f"Przyszedł użytkownik {member.name}")
+        now = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        embed=discord.Embed(title=member.name, description=" ", color=0x808000)
+        embed.add_field(name="NOWA PANDA !", value=f"Data: {now} ", inline=False)
+        logger.debug(f"Printing {embed}")
+        await panda_tools.channel_message(member.guild, embed, config["user_info_category_name"], embed=True)
+    except Exception as e:
+        logger.warning(e)
+        await panda_tools.channel_message(member.guild, e, "pandabot-log")
+
+@bot.event
+async def on_member_remove(member):
+    try:
+        logger.debug(f"Odszedł użytkownik {member.name}")
+        now = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        embed=discord.Embed(title=member.name, description=" ", color=0x8d8d8d)
+        embed.add_field(name="SAD PANDA ! Odszedł nam użytkownik", value=f"Data: {now} ", inline=False)
+        logger.debug(f"Printing {embed}")
+        await panda_tools.channel_message(member.guild, embed, config["user_info_category_name"], embed=True)
+    except Exception as e:
+        logger.warning(e)
+        await panda_tools.channel_message(member.guild, e, "pandabot-log")
